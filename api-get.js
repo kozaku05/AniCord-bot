0566359e7f6e8675b840b07da0105580420ad59f
Wataru Tsubokawa <111325821+nowf41@users.noreply.github.com>
2024-12-23 23:44:06 +0900

    変更されていない変数を定数にしました。

--- api-get.js
+++ api-get.js
@@ -1,19 +1,19 @@
 const axios = require("axios");
 const xml2js = require("xml2js");
-let url = "https://cal.syoboi.jp/db.php?Command=TitleLookup&Fields=Title&TID=";
+const url = "https://cal.syoboi.jp/db.php?Command=TitleLookup&Fields=Title&TID=";
 
 async function get(TID) {
   try {
-    let data = await axios.get(url + TID);
-    let xml = data.data;
-    let result = await xml2js.parseStringPromise(xml);
-    let Items = result?.TitleLookupResponse?.TitleItems?.[0]?.TitleItem?.[0];
+    const data = await axios.get(url + TID);
+    const xml = data.data;
+    const result = await xml2js.parseStringPromise(xml);
+    const Items = result?.TitleLookupResponse?.TitleItems?.[0]?.TitleItem?.[0];
     if (!Items) {
       return { error: "タイトルが見つかりませんでした" };
     }
-    let id = Items.$.id;
-    let title = Items.Title[0];
-    let response = { title: title, id: id };
+    const id = Items.$.id;
+    const title = Items.Title[0];
+    const response = { title: title, id: id };
     return response;
   } catch (error) {
     console.log(error);
